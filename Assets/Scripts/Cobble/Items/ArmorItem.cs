4a23751df2597b60d784dc4c70b7b05ca1ca6908
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-19 16:40:38 -0400

    Added Armor item

---
+++ Assets/Scripts/Cobble/Items/ArmorItem.cs
@@ -0,0 +1,31 @@
+﻿using Cobble.Lib;
+using UnityEngine;
+
+namespace Cobble.Items {
+    public class ArmorItem : Item {
+        private const string ItemIdValue = "armor";
+        private const string NameValue = "Armor";
+        private const int ItemMaxStack = 2;
+        private const string ItemSpritePath = "Images/Items/Armor Item";
+
+        public override string ItemId {
+            get { return ItemIdValue; }
+        }
+
+        public override string Name {
+            get { return NameValue; }
+        }
+
+        public override int MaxStack {
+            get { return ItemMaxStack; }
+        }
+
+        protected override string SpritePath {
+            get { return ItemSpritePath; }
+        }
+
+        public override void UseItem(GameObject usingGameObject) {
+            Debug.Log("Using Armor"); //TODO actually make this do something
+        }
+    }
+}
