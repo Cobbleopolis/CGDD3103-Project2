af1882cf5947973d12a5cd3cfbe4afa3efb22d7f
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-19 12:15:30 -0400

    Added health bar

---
+++ Assets/Scripts/Cobble/UI/HealthBarUi.cs
@@ -0,0 +1,19 @@
+﻿using Cobble.Entity;
+using UnityEngine;
+
+namespace Cobble.UI {
+    public class HealthBarUi : MonoBehaviour {
+
+        public LivingEntity LivingEntity;
+
+        [SerializeField]
+        private GameObject _remaningHealth;
+
+    
+        private void Update() {
+            var scale = _remaningHealth.transform.localScale;
+            scale.x = LivingEntity.GetHealthPercent();
+            _remaningHealth.transform.localScale = scale;
+        }
+    }
+}
