a2b7f275d5dec291706cb71d47dd7845f9c9fcab
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-21 12:25:40 -0400

    Added patrolling

---
+++ Assets/Scripts/Cobble/AI/PatrolAi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Cobble.AI {
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class PatrolAi : MonoBehaviour {
+
+        public Transform[] Checkpoints;
+
+        private NavMeshAgent _navMeshAgent;
+
+        private float _distanceThreshold = 0.1f;
+        
+        private int _currentCheckpoint;
+
+        private void Start() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _currentCheckpoint = -1;
+            GoToNextCheckPoint();
+        }
+
+        private void Update() {
+            if (_navMeshAgent.remainingDistance <= _distanceThreshold)
+                GoToNextCheckPoint();
+        }
+
+        private void GoToNextCheckPoint() {
+            _currentCheckpoint++;
+            if (_currentCheckpoint >= Checkpoints.Length)
+                _currentCheckpoint = 0;
+            _navMeshAgent.SetDestination(Checkpoints[_currentCheckpoint].position);
+        }
+
+    }
+}
