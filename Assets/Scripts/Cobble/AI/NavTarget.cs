6b8b77454cb7dcf93e3617dba3085d5269bb5eae
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-21 05:25:46 -0400

    Added basic level and AI

---
+++ Assets/Scripts/Cobble/AI/NavTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Cobble.AI {
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavTarget : MonoBehaviour {
+
+        public Transform Target;
+
+        private NavMeshAgent _navMeshAgent;
+
+        private void Start() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.SetDestination(Target.position);
+        }
+    }
+}
