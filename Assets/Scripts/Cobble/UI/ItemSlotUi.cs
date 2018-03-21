298fb0084427da0773ef67347e4ee2595be652f9
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-20 20:44:27 -0400

    Added item tooltips on mouseover

--- Assets/Scripts/Cobble/UI/ItemSlotUi.cs
+++ Assets/Scripts/Cobble/UI/ItemSlotUi.cs
@@ -1,10 +1,11 @@
 ﻿using Cobble.Player;
 using Cobble.Lib;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Cobble.UI {
-    public class ItemSlotUi : MonoBehaviour {
+    public class ItemSlotUi : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
         public PlayerInventory PlayerInventory;
 
         public int SlotNumber;
@@ -15,8 +16,13 @@ namespace Cobble.UI {
 
         [SerializeField] private Image _itemImage;
 
+        private TooltipUi _tooltip;
 
-        private void Start() { }
+        private bool _isTooltipShown;
+
+        private void Start() {
+            _tooltip = FindObjectOfType<TooltipUi>();
+        }
 
         public void UpdateInfo() {
             if (!PlayerInventory)
@@ -26,11 +32,33 @@ namespace Cobble.UI {
                 _itemImage.enabled = false;
                 _itemImage.overrideSprite = null;
                 _itemText.text = "";
+                if (!_isTooltipShown || !_tooltip) return;
+                _isTooltipShown = false;
+                _tooltip.Hide();
             } else {
                 _itemImage.enabled = true;
                 _itemImage.overrideSprite = _itemStack.Item.ItemSprite;
                 _itemText.text = _itemStack.Item.MaxStack > 1 ? _itemStack.Amount.ToString() : "";
+                if (_isTooltipShown)
+                    SetTooltipText();
             }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            if (_tooltip == null || PlayerInventory.IsSlotEmpty(SlotNumber)) return;
+            _isTooltipShown = true;
+            _tooltip.Show();
+            SetTooltipText();
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            _tooltip.Hide();
+            _isTooltipShown = false;
+        }
+
+        private void SetTooltipText() {
+            if (!_tooltip) return;
+            _tooltip.SetText((_itemStack == null || _itemStack.Item == null) ? "" : _itemStack.Item.Name);
         }
 
         public void UseItem() {
---
+++ Assets/Scripts/Cobble/UI/TooltipUi.cs
@@ -0,0 +1,49 @@
+﻿using Cobble.Core;
+using Cobble.Lib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cobble.UI {
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(CanvasGroup))]
+    public class TooltipUi : MonoBehaviour {
+        
+        public Vector3 Offset = new Vector3(5f, 0f, 0f);
+
+        private GuiManager _guiManager;
+
+        private CanvasGroup _canvasGroup;
+
+        private Text _tooltipText;
+
+        public bool IsShown {
+            get { return _canvasGroup.alpha > float.Epsilon; }
+        }
+
+        private void Start() {
+            _guiManager = FindObjectOfType<GuiManager>();
+            _canvasGroup = GetComponent<CanvasGroup>();
+            _tooltipText = GetComponentInChildren<Text>();
+        }
+
+        private void Update() {
+            if (_guiManager == null || _guiManager.GetCurrentGuiScreen() == GuiScreen.None || !IsShown) return;
+            transform.position = Input.mousePosition + Offset;
+        }
+
+        public void SetText(string tooltipText) {
+            if (!_tooltipText) return;
+            _tooltipText.text = tooltipText;
+        }
+
+        public void Show() {
+            _canvasGroup.alpha = 1;
+            transform.position = Input.mousePosition;
+        }
+
+        public void Hide() {
+            _canvasGroup.alpha = 0;
+        }
+        
+    }
+}
