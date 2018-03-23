1e29b524ff239fb83906e3c7a9987575a90f737e
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-23 03:50:46 -0400

    Began adding AI

---
+++ Assets/Scripts/Cobble/AI/FollowAi.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Cobble.AI {
+    
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class FollowAi : MonoBehaviour {
+
+        public Transform FollowTarget;
+
+        public float FollowDistance = 5f;
+
+        private NavMeshAgent _navMeshAgent;
+        
+        private void Start() {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update() {
+            if (!FollowTarget) return;
+            _navMeshAgent.isStopped = _navMeshAgent.remainingDistance <= FollowDistance;
+            _navMeshAgent.SetDestination(FollowTarget.position);
+        }
+    }
+}
