964c9a02d62e81c917dce7be2355ef59e051da0d
Cobbleopolis <cobbleopolis@gmail.com>
2018-03-24 02:45:26 -0400

    Added max lifetime to bullets

--- Assets/Scripts/Cobble/Projectile/BulletBehavior.cs
+++ Assets/Scripts/Cobble/Projectile/BulletBehavior.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 namespace Cobble.Projectile {
     public class BulletBehavior : MonoBehaviour {
 
+        public float MaxLifetime = 10f;
+
         public float DamageAmount = 10f;
+
+        private void Start() {
+            Destroy(gameObject, MaxLifetime);
+        }
 
         private void OnCollisionEnter(Collision other) {
             var livingEntity = other.gameObject.GetComponent<LivingEntity>();
